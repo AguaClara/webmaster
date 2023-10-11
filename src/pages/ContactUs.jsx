cea18d8f217a054b5123fad590b43626e628b09a
anya-yerramilli <anya.yerramilli@gmail.com>
2023-10-10 23:59:45 -0400

    Implemented basic contact us page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import groupPic from "../assets/img/group_pic.jpg";
 
 function ContactUs() {
   return (
@@ -7,8 +9,30 @@ function ContactUs() {
       <Navbar />
       <div className="mx-auto max-w-screen-xl ">
         <div className=" text-5xl mt-[5%]">Questions?</div>
-        <img></img>
-      </div>
+        <div className=" text-3xl mt-[3%]"></div>
+        <div className="flex flex-row justify-between w-full text-center ">
+          <div className="flex-1"><img src={groupPic} alt="React Image" /></div>
+          <div className="flex-1">
+            <div className=" text-10xl mt-[10%]"></div>
+            <div className=" text-3xl mt-[3%]">Contact Us</div>
+            <div className=" text-1xl mt-[1%]">Connect with us on...</div>
+            <div className=" text-5xl mt-[5%]"></div>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/company/aguaclara-at-cornell/" rel="noreferrer">
+                  LinkedIn
+                </a></li>
+              <li><a href="https://github.com/AguaClara" rel="noreferrer">
+                GitHub
+              </a></li>
+              <li><a href="https://www.instagram.com/cuaguaclara/" rel="noreferrer">
+                Instagram
+              </a></li>
+            </ul></div>
+        </div>
+
+      </div >
+      <Footer />
     </>
   );
 }
