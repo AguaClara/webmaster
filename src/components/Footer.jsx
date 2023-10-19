1d24b58a22eb30817e6be26fffc37f926e357c9b
aasha-yeluri <126119681+aasha-yeluri@users.noreply.github.com>
2023-10-19 13:43:42 -0400

    Updated Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,43 @@
-import React from "react";
-
-function Footer() {
+import './footer.css';
+import AguaClara from "../assets/img/AguaClara_Logo.png";
+const Footer = () => {
   return (
-    <footer className="bg-[#28C3EE] text-white text-center py-4 mt-[5%]">
-      <div className="container mx-auto">
-        <p>&copy; 2023 AguaClara Cornell</p>
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          
+         
+            <img src={AguaClara} alt="" />
+          
+          
+        </div>
+        <div className="footer-middle">
+          <h3 className="footer-heading">Contact</h3>
+          <p className="footer-text">
+            <a className='inst' href="https://www.instagram.com/cuaguaclara/">Instagram</a>
+            <br />
+            <a className='inst' href="https://www.linkedin.com/company/aguaclara-at-cornell/">LinkedIn</a>
+            <br />
+            <a className='inst' href="https://www.facebook.com/CUAguaClara/">Facebook</a>
+            <br />
+            <a className='inst' href="https://github.com/AguaClara">GitHub</a>
+          </p>
+        </div>
+        <div className="footer-right">
+          <h3 className="footer-heading">Donate</h3>
+          <a href="https://www.aguaclara.org/donate/" className="footer-button">
+            Donate Now
+          </a>
+        
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <p className="footer-copyright-text">
+          &copy; 2023 AguaClara Cornell
+        </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
