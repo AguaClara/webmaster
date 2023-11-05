22eb3bf2aee3fd56cac1037569096081486c390c
Dora <cw749@cornell.edu>
2023-11-05 01:17:32 -0500

    removed duplicate watermark in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import AguaClara from "../assets/img/AguaClara_Logo.png";
 const Footer = () => {
   return (
     <footer className="bg-blue-300 text-white text-center py-4 mt-20">
-      <div className="container mx-auto">
+      {/* <div className="container mx-auto">
         <p>&copy; 2023 AguaClara Cornell</p>
-      </div>
+      </div> */}
       <div className="container mx-auto flex justify-between items-center mt-5">
         <div className="w-1/3">
           <img src={AguaClara} alt="" />
@@ -43,9 +43,8 @@ const Footer = () => {
           </p>
         </div>
         <div className="w-1/3">
-          <h3 className="text-2xl font-semibold">Donate</h3>
           <a
-            href="https://www.aguaclara.org/donate/"
+            href="/Donate"
             className="bg-white text-blue-500 hover:bg-blue-300 hover:text-white px-4 py-2 rounded-full font-semibold"
           >
             Donate Now
