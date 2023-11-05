5992f5c18865d646560210b9f66d52491ddab7ec
aasha-yeluri <126119681+aasha-yeluri@users.noreply.github.com>
2023-11-04 20:40:26 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,35 @@
+import './footer.css';
 import AguaClara from "../assets/img/AguaClara_Logo.png";
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer className="bg-gray-100 py-6 text-black">
-      <div className="container mx-auto px-4 flex justify-between items-center flex-wrap">
-
-        {/* Left - Logo */}
-        <div className="flex-shrink-0 mb-4 md:mb-0">
-          <img src={AguaClara} alt="AguaClara Logo" className="w-64" /> {/* Adjusted size here */}
+    <footer className="bg-blue-300 text-white text-center py-4 mt-20">
+      <div className="container mx-auto">
+        <p>&copy; 2023 AguaClara Cornell</p>
+      </div>
+      <div className="container mx-auto flex justify-between items-center mt-5">
+        <div className="w-1/3">
+          <img src={AguaClara} alt="" />
         </div>
-
-        {/* Middle - Contact */}
-        <div className="flex flex-col items-start mb-4 md:mb-0 md:ml-12">
-          <h3 className="text-lg font-semibold mb-2">Contact</h3>
-          <a className="text-black hover:underline mb-1" href="https://www.instagram.com/cuaguaclara/">Instagram</a>
-          <a className="text-black hover:underline mb-1" href="https://www.linkedin.com/company/aguaclara-at-cornell/">LinkedIn</a>
-          <a className="text-black hover:underline mb-1" href="https://www.facebook.com/CUAguaClara/">Facebook</a>
-          <a className="text-black hover:underline" href="https://github.com/AguaClara">GitHub</a>
+        <div className="w-1/3">
+          <h3 className="text-2xl font-semibold">Contact</h3>
+          <p>
+            <a className="text-blue-500 hover:text-blue-700" href="https://www.instagram.com/cuaguaclara/">Instagram</a>
+            <br />
+            <a className="text-blue-500 hover:text-blue-700" href="https://www.linkedin.com/company/aguaclara-at-cornell/">LinkedIn</a>
+            <br />
+            <a className="text-blue-500 hover:text-blue-700" href="https://www.facebook.com/CUAguaClara/">Facebook</a>
+            <br />
+            <a className="text-blue-500 hover:text-blue-700" href="https://github.com/AguaClara">GitHub</a>
+          </p>
         </div>
-
-        {/* Right - Donate */}
-        <div className="flex flex-col items-start ml-12">
-          <h3 className="text-lg font-semibold mb-2">Donate</h3>
-          <a href="https://www.aguaclara.org/donate/" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded">
-            Donate Now
-          </a>
+        <div className="w-1/3">
+          <h3 className="text-2xl font-semibold">Donate</h3>
+          <a href="https://www.aguaclara.org/donate/" className="bg-white text-blue-500 hover:bg-blue-300 hover:text-white px-4 py-2 rounded-full font-semibold">Donate Now</a>
         </div>
       </div>
-
-      {/* Copyright */}
-      <div className="mt-6 text-center">
-        <p>&copy; 2023 AguaClara Cornell</p>
+      <div className="bg-blue-300 py-2">
+        <p className="text-white text-sm">&copy; 2023 AguaClara Cornell</p>
       </div>
     </footer>
   );
